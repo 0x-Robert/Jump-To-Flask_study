3fb7eb8b9942f3cdcf4a1c14d2db374161ff217a
shypang <smartdragon417@gmail.com>
2021-12-31 09:57:37 +0900

    질문 수정,삭제 추가

--- pybo/models.py
+++ pybo/models.py
@@ -25,6 +25,9 @@ class Question(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('question_set'))
 
+    modify_date = db.Column(db.DateTime(),nullable=True)
+
+
 
 
 class Answer(db.Model):
@@ -43,7 +46,7 @@ class Answer(db.Model):
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user=db.relationship('User', backref=db.backref('answer_set'))
-
+    modify_date = db.Column(db.DateTime(), nullable=True)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Blueprint, render_template, request, url_for,g
+from flask import Blueprint, render_template, request, url_for,g, flash
 from werkzeug.utils import redirect
 from .. import db
 from ..forms import QuestionForm, AnswerForm
@@ -24,6 +24,8 @@ def detail(question_id):
     question=Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html',question=question,form=form)
 
+
+
 @bp.route('/create/', methods=('GET','POST'))
 @login_required
 def create():
@@ -34,3 +36,37 @@ def create():
         db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('question/question_form.html',form=form)
+
+
+
+
+
+@bp.route('/modify/<int:question_id>',methods=('GET','POST'))
+@login_required
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('수정권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=question_id))
+    if request.method == 'POST': #POST 요청
+        form = QuestionForm()
+        if form.validate_on_submit():
+            form.populate_obj(question)
+            question.modify_date = datetime.now() #수정일시 저장
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=question_id))
+    else: #GET요청
+        form = QuestionForm(obj=question)
+    return render_template('question/question_form.html',form=form)
+
+
+@bp.route('/delete/<int:question_id>')
+@login_required
+def delete(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('삭제 권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=question_id))
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('question.list'))
