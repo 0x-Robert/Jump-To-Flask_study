1026d53315d5cf52766fe355474785b8f8106f92
Ubuntu <ubuntu@ip-172-31-5-68.us-east-2.compute.internal>
2022-01-08 16:15:52 +0900

    db를 postgresql로 변경

--- config/production.py
+++ config/production.py
@@ -1,9 +1,14 @@
 from config.default import *
 from logging.config import dictConfig
 
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+        user='dbmasteruser',
+        pw='yongyong417',
+        url='localhost',
+        db='flask_pybo')
 
 
-SQL_ALCHEMY_DATABASE_URI='sqlite:///{}'.format(os.path.join(BASE_DIR,'pybo.db'))
+#SQL_ALCHEMY_DATABASE_URI='sqlite:///{}'.format(os.path.join(BASE_DIR,'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 # SECRET_KEY=b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
 SECRET_KEY=b'\x9f}\xe7\x85\xee\xcc\r.2g\xa8\xf0?\xd0\t\xd9'
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,14 +1,20 @@
 from datetime import datetime
-from flask import Blueprint, render_template, request, url_for,g, flash
+from flask import Blueprint, render_template, request, url_for,g, flash,current_app
 from werkzeug.utils import redirect
 from .. import db
 from ..forms import QuestionForm, AnswerForm
 from pybo.models import Question ,Answer, User , question_voter
-from sqlalchemy import func
+from sqlalchemy import func, nullslast
 from pybo.views.auth_views import login_required
 
 bp = Blueprint('question', __name__ , url_prefix='/question')
 
+
+def _nullslast(obj):
+    if current_app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        return obj
+    else:
+        return nullslast(obj)
 
 @bp.route('/list/')
 def _list():
@@ -23,13 +29,13 @@ def _list():
             .group_by(question_voter.c.question_id).subquery()
         question_list = Question.query \
             .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
-            .order_by(sub_query.c.num_voter.desc(), Question.create_date.desc())
+            .order_by(_nullslast(sub_query.c.num_voter.desc()), Question.create_date.desc())
     elif so == 'popular':
         sub_query = db.session.query(Answer.question_id, func.count('*').label('num_answer')) \
             .group_by(Answer.question_id).subquery()
         question_list = Question.query \
             .outerjoin(sub_query, Question.id == sub_query.c.question_id) \
-            .order_by(sub_query.c.num_answer.desc(), Question.create_date.desc())
+            .order_by(_nullslast(sub_query.c.num_answer.desc()), Question.create_date.desc())
     else:  # recent
         question_list = Question.query.order_by(Question.create_date.desc())
 
